6036732346ac76642b86bbea11d4b4d74eca1076
Glenn Guinoban <123550094+glngnbn@users.noreply.github.com>
2024-05-11 23:21:25 +0800

    Add files via upload

---
+++ Code/Scan.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace DWITIC
+{
+    partial class Scan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Scan));
+            this.textBoxSDG = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.picBoxSDG = new System.Windows.Forms.PictureBox();
+            this.picBoxUpload = new System.Windows.Forms.PictureBox();
+            this.btnClassify = new System.Windows.Forms.Button();
+            this.btnUploadNow = new System.Windows.Forms.Button();
+            this.btnReturn = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxSDG)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxUpload)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxSDG
+            // 
+            this.textBoxSDG.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSDG.Location = new System.Drawing.Point(446, 514);
+            this.textBoxSDG.Multiline = true;
+            this.textBoxSDG.Name = "textBoxSDG";
+            this.textBoxSDG.ReadOnly = true;
+            this.textBoxSDG.Size = new System.Drawing.Size(300, 76);
+            this.textBoxSDG.TabIndex = 19;
+            this.textBoxSDG.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(446, 136);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(718, 35);
+            this.textBox2.TabIndex = 18;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(764, 190);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(400, 400);
+            this.textBox1.TabIndex = 17;
+            // 
+            // picBoxSDG
+            // 
+            this.picBoxSDG.BackColor = System.Drawing.Color.Transparent;
+            this.picBoxSDG.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.picBoxSDG.Location = new System.Drawing.Point(446, 190);
+            this.picBoxSDG.Name = "picBoxSDG";
+            this.picBoxSDG.Size = new System.Drawing.Size(300, 300);
+            this.picBoxSDG.TabIndex = 16;
+            this.picBoxSDG.TabStop = false;
+            // 
+            // picBoxUpload
+            // 
+            this.picBoxUpload.BackColor = System.Drawing.Color.Transparent;
+            this.picBoxUpload.BackgroundImage = global::DWITIC.Properties.Resources.icons8_photoscan_60;
+            this.picBoxUpload.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.picBoxUpload.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.picBoxUpload.Location = new System.Drawing.Point(26, 190);
+            this.picBoxUpload.Name = "picBoxUpload";
+            this.picBoxUpload.Size = new System.Drawing.Size(400, 400);
+            this.picBoxUpload.TabIndex = 15;
+            this.picBoxUpload.TabStop = false;
+            // 
+            // btnClassify
+            // 
+            this.btnClassify.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClassify.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClassify.Image = global::DWITIC.Properties.Resources.icons8_catalog_25;
+            this.btnClassify.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnClassify.Location = new System.Drawing.Point(238, 131);
+            this.btnClassify.Name = "btnClassify";
+            this.btnClassify.Padding = new System.Windows.Forms.Padding(3, 0, 0, 0);
+            this.btnClassify.Size = new System.Drawing.Size(135, 44);
+            this.btnClassify.TabIndex = 14;
+            this.btnClassify.Text = "   IDENTIFY";
+            this.btnClassify.UseVisualStyleBackColor = true;
+            this.btnClassify.Click += new System.EventHandler(this.btnClassify_Click);
+            // 
+            // btnUploadNow
+            // 
+            this.btnUploadNow.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnUploadNow.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUploadNow.Image = global::DWITIC.Properties.Resources.icons8_scan_30;
+            this.btnUploadNow.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnUploadNow.Location = new System.Drawing.Point(81, 131);
+            this.btnUploadNow.Name = "btnUploadNow";
+            this.btnUploadNow.Padding = new System.Windows.Forms.Padding(12, 0, 0, 0);
+            this.btnUploadNow.Size = new System.Drawing.Size(135, 44);
+            this.btnUploadNow.TabIndex = 13;
+            this.btnUploadNow.Text = "   SCAN";
+            this.btnUploadNow.UseVisualStyleBackColor = true;
+            this.btnUploadNow.Click += new System.EventHandler(this.btnUploadNow_Click);
+            // 
+            // btnReturn
+            // 
+            this.btnReturn.BackColor = System.Drawing.Color.Transparent;
+            this.btnReturn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnReturn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnReturn.FlatAppearance.BorderSize = 0;
+            this.btnReturn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnReturn.Image = global::DWITIC.Properties.Resources.icons8_back_50;
+            this.btnReturn.Location = new System.Drawing.Point(12, 52);
+            this.btnReturn.Name = "btnReturn";
+            this.btnReturn.Size = new System.Drawing.Size(55, 55);
+            this.btnReturn.TabIndex = 12;
+            this.btnReturn.UseVisualStyleBackColor = false;
+            this.btnReturn.Click += new System.EventHandler(this.btnReturn_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.pictureBox2);
+            this.panel3.Location = new System.Drawing.Point(0, -3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1204, 42);
+            this.panel3.TabIndex = 20;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Palatino Linotype", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(50, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(142, 23);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "DWITIC | SCAN";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox2.BackgroundImage = global::DWITIC.Properties.Resources.icons8_scan_30;
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox2.Location = new System.Drawing.Point(15, 8);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(30, 30);
+            this.pictureBox2.TabIndex = 28;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = global::DWITIC.Properties.Resources.icons8_close_15;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(1151, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(22, 22);
+            this.button1.TabIndex = 28;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Scan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(120F, 120F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::DWITIC.Properties.Resources._440054786_792038046192240_5215925064332758294_n;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1188, 656);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.textBoxSDG);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.picBoxSDG);
+            this.Controls.Add(this.picBoxUpload);
+            this.Controls.Add(this.btnClassify);
+            this.Controls.Add(this.btnUploadNow);
+            this.Controls.Add(this.btnReturn);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Scan";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "DWITIC | SCAN";
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxSDG)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxUpload)).EndInit();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxSDG;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox picBoxSDG;
+        private System.Windows.Forms.PictureBox picBoxUpload;
+        private System.Windows.Forms.Button btnClassify;
+        private System.Windows.Forms.Button btnUploadNow;
+        private System.Windows.Forms.Button btnReturn;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+    }
+}
---
+++ Code/Scan.cs
@@ -0,0 +1,165 @@
+﻿using Emgu.CV;
+using Emgu.CV.Structure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DWITIC
+{
+    public partial class Scan : Form
+    {
+        bool _streaming;
+        Capture _capture;
+
+        public Scan()
+        {
+            InitializeComponent();
+            this.BackColor = Color.White;
+            this.Size = new Size(1188, 656);
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            GraphicsPath path = new GraphicsPath();
+            int radius = 20;
+            path.AddArc(0, 0, radius, radius, 180, 90);
+            path.AddArc(this.Width - radius, 0, radius, radius, 270, 90);
+            path.AddArc(this.Width - radius, this.Height - radius, radius, radius, 0, 90);
+            path.AddArc(0, this.Height - radius, radius, radius, 90, 90);
+            path.CloseFigure();
+
+            this.Region = new Region(path);
+
+        }
+
+        private void Scan_Load(object sender, EventArgs e)
+        {
+            _streaming = true;
+            StartCamera();
+        }
+
+        private void StartCamera()
+        {
+            _capture = new Capture();
+            Application.Idle += ProcessFrame;
+        }
+
+        private void StopCamera()
+        {
+            if (_capture != null)
+            {
+                Application.Idle -= ProcessFrame;
+                _capture.Dispose();
+            }
+        }
+
+        private void btnUploadNow_Click(object sender, EventArgs e)
+        {
+            if (_streaming)
+            {
+                CaptureImage();
+                StopCamera();
+                _streaming = false;
+            }
+            else
+            {
+                StartCamera();
+                _streaming = true;
+            }
+        }
+
+        private void CaptureImage()
+        {
+            var img = _capture.QueryFrame().ToImage<Bgr, byte>();
+            var bmp = img.Bitmap;
+            picBoxUpload.Image = bmp;
+        }
+
+        private void ProcessFrame(object sender, EventArgs e)
+        {
+            if (_streaming)
+            {
+                CaptureImage();
+            }
+        }
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            StopCamera();
+            base.OnFormClosing(e);
+        }
+
+        private void btnClassify_Click(object sender, EventArgs e)
+        {
+            if (picBoxUpload.Image == null)
+            {
+                MessageBox.Show("Please capture an image first.");
+                return;
+            }
+
+            byte[] imageBytes;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                picBoxUpload.Image.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                imageBytes = ms.ToArray();
+            }
+
+            DWITIC.ScanModel.ModelInput sampleData = new DWITIC.ScanModel.ModelInput()
+            {
+                ImageSource = imageBytes,
+            };
+
+            var result = DWITIC.ScanModel.Predict(sampleData);
+
+            var labelInfo = LabelData.Labels.FirstOrDefault(label => label.Label.Equals(result.PredictedLabel, StringComparison.OrdinalIgnoreCase));
+
+            if (labelInfo != null)
+            {
+                string message = $"{labelInfo.Label} {labelInfo.Classification}";
+                string info = $"{labelInfo.Description}";
+                string sdg = $"{labelInfo.SDG}";
+
+                foreach (string imagePath in labelInfo.ImagePaths)
+                {
+                    if (File.Exists(imagePath))
+                    {
+                        picBoxSDG.Image = Image.FromFile(imagePath);
+                    }
+                    else
+                    {
+                        MessageBox.Show($"Image not found: {imagePath}");
+                    }
+                }
+                textBox1.Text = info;
+                textBox2.Text = message;
+                textBoxSDG.Text = sdg;
+            }
+            else
+            {
+                MessageBox.Show("Label information not found.");
+            }
+        }
+
+        private void btnReturn_Click(object sender, EventArgs e)
+        {
+            Main main = new Main();
+            main.Show();
+
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
